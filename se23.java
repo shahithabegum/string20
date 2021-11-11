fb2a07fb608df1737d6ed6b066f2ff02b577d266
shahithabegum <69446892+shahithabegum@users.noreply.github.com>
2021-11-11 16:02:24 +0530

    Add files via upload

---
+++ se23.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+class se23
+{
+  public static void main(String arg[])
+   {
+     try
+         {
+            String s ="Stephen Edwin King";
+            String s1 ="Stephen Edwin King";
+	
+	    System.out.println(s.equalsIgnoreCase(s1));
+	  
+	}
+	catch(Exception e)
+	{
+	  System.out.println(e);
+	}
+       }
+}
